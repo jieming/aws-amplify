e2b58023f406a3aa2b3154a957bf7b2c95f945e7
Jieming Liu <jieming.liu@juralio.com>
2019-06-23 17:42:44 +0100

    Add lambda and api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator } from "aws-amplify-react";
+import { API } from "aws-amplify";
 
 function App() {
+  async const callApi = () => {
+    try {
+      const peopleData = await API.get('mainappapi', '/people')
+      console.log('peopleData: ', peopleData)
+    } catch(error) {
+      console.log('error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    callApi()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
