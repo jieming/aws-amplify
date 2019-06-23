b6ef94c5708b742e9d1261f0f8ca17087696432d
Jieming Liu <jieming.liu@juralio.com>
2019-06-23 17:44:59 +0100

    fix a function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState }from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator } from "aws-amplify-react";
 import { API } from "aws-amplify";
 
 function App() {
-  async const callApi = () => {
+  async function callApi() {
     try {
-      const peopleData = await API.get('mainappapi', '/people')
-      console.log('peopleData: ', peopleData)
-    } catch(error) {
-      console.log('error: ', error)
+      const peopleData = await API.get("mainappapi", "/people");
+      console.log("peopleData: ", peopleData);
+    } catch (error) {
+      console.log("error: ", error);
     }
   }
 
   useEffect(() => {
-    callApi()
-  }, [])
+    callApi();
+  }, []);
 
   return (
     <div className="App">
