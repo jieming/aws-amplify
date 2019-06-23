1f646a4ba021b56c40f0ef49e7f976a1d79f5e78
Jieming Liu <jieming.liu@juralio.com>
2019-06-23 17:49:31 +0100

    Set people in state

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { withAuthenticator } from "aws-amplify-react";
 import { API } from "aws-amplify";
 
 function App() {
+  const [people, updatePeople] = useState([]);
+
   async function callApi() {
     try {
       const peopleData = await API.get("mainappapi", "/people");
       console.log("peopleData: ", peopleData);
+      updatePeople(peopleData.people);
     } catch (error) {
       console.log("error: ", error);
     }
@@ -23,6 +26,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to Cloudtop!</h2>
+        {people.map((people, i) => (
+          <h2>{people.name}</h2>
+        ))}
       </header>
     </div>
   );
